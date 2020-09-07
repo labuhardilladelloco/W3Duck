6ef188c8a7e5678d36bff7d156b5f50ecc497e66
LaBuhardillaDelLoco <70807575+labuhardilladelloco@users.noreply.github.com>
2020-09-07 21:04:07 +0200

    Update Server.hpp

--- W3Duck!/WEMOS/Server.hpp
+++ W3Duck!/WEMOS/Server.hpp
@@ -11,8 +11,16 @@ void inicio(){
   server.send(200, "text/html",s );
 }
 
+
+
+void comUsb(){
+ String cmd = server.arg(String("cmd"));
+ Serial.print(cmd);
+}
+
 void InitServer(){
    server.on("/", inicio);
+   server.on("/prueba", comUsb); 
    server.onNotFound(handleNotFound);
    server.begin();
    Serial.println("HTTP server started");
